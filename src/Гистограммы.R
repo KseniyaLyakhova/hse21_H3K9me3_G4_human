6aa91db3b70fc07a5a93925f6355dd1d8e8c8d86
KseniyaLyakhova <85398612+KseniyaLyakhova@users.noreply.github.com>
2021-06-09 16:39:19 +0300

    построение гистограмм

---
+++ src/Гистограммы.R
@@ -0,0 +1,33 @@
+
+
+library(ggplot2)
+library(dplyr)
+
+###
+
+NAME <- 'G4.merge'
+#NAME <- 'H3K9me3_A549_ENCFF444EWQ.hg19'
+#NAME <- 'H3K9me3_A549_ENCFF444EWQ.h38'
+#NAME <- 'H3K9me3_A549_ENCFF811QUJ.hg19'
+#NAME <- 'H3K9me3_A549_ENCFF811QUJ.h38'
+
+OUT_DIR <- 'D:/ДЗ/3 курс/Майнор/Проект/Results/'
+
+###
+
+bed_df <- read.delim(paste0('D:/ДЗ/3 курс/Майнор/Проект/', NAME, '.bed'), as.is = TRUE, header = FALSE)
+head (bed_df)
+
+
+colnames(bed_df) <- c('chrom', 'start', 'end')
+
+head (bed_df)
+bed_df$len <- bed_df$end - bed_df$start
+head (bed_df)
+
+ggplot(bed_df) +
+    aes(x = len) +
+    geom_histogram() +
+    ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+    theme_bw()
+ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
